7fe00185ce47ce598771fa1e86202e60dfd3573f
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2019-05-24 12:59:48 -0300

    Separate content to another component, add logout

---
+++ src/Content.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+const Content = () => (
+  <div>
+    <h1>This a totally normal blog</h1>
+    <h2>Look, I'm a smaller header</h2>
+    <p>
+      Sed non consectetur salami. Pancetta frankfurter boudin turducken bresaola dolore bacon irure minim pork chop
+      nulla do. Bresaola adipisicing velit sint beef. Ham hock do rump flank pastrami id exercitation laborum beef
+      nostrud cow.
+    </p>
+    <p>
+      <img style={{float: 'right'}} src='https://media.giphy.com/media/KOVlHmbBA09XO/giphy.gif' width='480'
+           height='230' alt='hello there'/>
+      Ut adipisicing pork porchetta. Pork aute qui bresaola turducken andouille, deserunt do. Brisket sed tongue
+      strip
+      steak labore do deserunt ham buffalo hamburger. Esse laborum qui drumstick et in voluptate doner short ribs
+      pork
+      loin tail buffalo eu id. Officia et est, biltong fugiat enim short ribs. Prosciutto ball tip sint proident,
+      chuck short ribs ham hock meatball ea filet mignon jerky ut strip steak in in.
+      <br/>
+      <br/>
+      Nunc pulvinar lacus id purus ultrices id sagittis neque convallis. Nunc vel libero orci. Vivamus at dolor a
+      nibh
+      aliquet luctus. Duis imperdiet mi id lorem pellentesque tempus. Ut quis felis posuere tellus feugiat ultrices.
+      Praesent id tortor non tellus viverra placerat. Vivamus euismod nisi a leo interdum molestie ornare tellus
+      consectetur.
+      <br/>
+      <br/>
+      Nullam cursus varius tellus, id laoreet magna hendrerit at. Nulla sit amet purus at nunc blandit ultricies eu
+      a
+      urna. Proin euismod porttitor neque id ultricies. Proin faucibus convallis elit. Maecenas rhoncus arcu at
+      arcu.
+      Proin libero. Proin adipiscing. In quis lorem vitae elit consectetuer pretium. Nullam ligula urna, adipiscing
+      nec, iaculis ut, elementum non, turpis. Fusce pulvinar.
+    </p>
+    <h2>Oh boy another header</h2>
+    <p>
+      Sed ullamcorper morbi tincidunt ornare massa eget. Convallis posuere morbi leo urna molestie at elementum eu
+      facilisis. In ante metus dictum at tempor commodo. Sit amet consectetur adipiscing elit pellentesque habitant
+      morbi. Condimentum mattis pellentesque id nibh tortor id aliquet lectus proin. Suspendisse ultrices gravida
+      dictum fusce ut. Ac turpis egestas integer eget aliquet. Pellentesque eu tincidunt tortor aliquam nulla
+      facilisi. Tincidunt vitae semper quis lectus nulla at. Mauris ultrices eros in cursus turpis. Commodo odio
+      aenean sed adipiscing. Mauris sit amet massa vitae. Enim nec dui nunc mattis enim ut tellus elementum. Dictum
+      at
+      tempor commodo ullamcorper. At lectus urna duis convallis convallis. Adipiscing diam donec adipiscing
+      tristique
+      risus nec feugiat.
+    </p>
+    <p>
+      Integer vitae justo eget magna fermentum. Egestas dui id ornare arcu odio ut sem. Et ultrices neque ornare
+      aenean. Enim sit amet venenatis urna cursus eget nunc. Tellus pellentesque eu tincidunt tortor aliquam nulla.
+      Volutpat odio facilisis mauris sit amet. Amet justo donec enim diam vulputate ut pharetra sit.
+    </p>
+  </div>
+);
+
+export default Content;
